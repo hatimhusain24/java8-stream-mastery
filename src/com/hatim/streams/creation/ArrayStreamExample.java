54ea84dbac62f270e7a1194ecd5a25f8da9870ec
Hatim Husain <hatim.husain24@gmail.com>
2026-06-18 23:38:49 +0530

    Stream generation : Array stream example

---
+++ src/com/hatim/streams/creation/ArrayStreamExample.java
@@ -0,0 +1,10 @@
+package com.hatim.streams.creation;
+
+import java.util.Arrays;
+
+public class ArrayStreamExample {
+    public static void main(String[] args) {
+        int[] arr ={10,20,30,40,50};
+        Arrays.stream(arr).forEach(System.out::println);
+    }
+}
