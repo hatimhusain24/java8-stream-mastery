997f4ae998400633d501c48e6aefc9c6c65643d4
Hatim Husain <hatim.husain24@gmail.com>
2026-06-18 23:38:35 +0530

    Stream generation : Set stream example

---
+++ src/com/hatim/streams/creation/SetStreamExample.java
@@ -0,0 +1,12 @@
+package com.hatim.streams.creation;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Set;
+
+public class SetStreamExample {
+    public static void main(String[] args) {
+        Set<String> set = Set.of("Husain","Hatim");
+        set.stream().forEach(System.out::println);
+    }
+}
