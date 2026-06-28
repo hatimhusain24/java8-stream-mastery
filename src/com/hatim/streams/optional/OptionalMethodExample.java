ca5324d38b9f7ed3d59498760a792643a0f214cc
Hatim Husain <hatim.husain24@gmail.com>
2026-06-29 00:53:15 +0530

    Optional Methods : examples solved

--- src/com/hatim/streams/optional/OptionalMethodExample.java
+++ src/com/hatim/streams/optional/OptionalMethodExample.java
@@ -1,12 +1,15 @@
 package com.hatim.streams.optional;
 
+import com.hatim.streams.Employee;
+
+import java.util.List;
 import java.util.Optional;
 
 public class OptionalMethodExample {
     public static void main(String[] args) throws Exception {
         // 1. Create Optional using: Optional.of()
         Optional<String> optional = Optional.of("Hello");
-        System.out.println(optional.get());
+        System.out.println(optional);
 
         // 2. Create Optional using: Optional.ofNullable()
         Optional<String> optional2 = Optional.ofNullable(null);
@@ -17,7 +20,8 @@ public class OptionalMethodExample {
         System.out.println(optional3);
 
         // 4. Check whether value exists
-        Optional.of("Java").ifPresent(System.out::println);
+        boolean result4 = Optional.of("Java").isPresent();
+        System.out.println(result4);
 
         // 5. Check whether value is empty
         boolean optional5 = Optional.ofNullable(5).isEmpty();
@@ -28,7 +32,7 @@ public class OptionalMethodExample {
         System.out.println(optional6.get());
 
         // 7. Use: orElse()
-        String optional7 = Optional.empty().orElse("unknown").toString();
+        String optional7 = Optional.<String>empty().orElse("unknown");
         System.out.println(optional7);
 
         // 8. Use: orElseGet()
@@ -37,5 +41,18 @@ public class OptionalMethodExample {
          // 9. Use: orElseThrow()
         String result9 = Optional.of("Java").orElseThrow(Exception::new);
         System.out.println(result9);
+
+        // 10. Filter employee list. Find employee id 10
+        List<Employee> employees = List.of(
+                new Employee(1,"Adam",28,"IT",40000,List.of("Java", "Spring Boot", "SQL")),
+                new Employee(2,"Noah",32,"HR",50000,List.of("Excel", "Communication", "Recruitment")),
+                new Employee(3,"Abraham",35,"IT",60000,List.of("Java", "Kafka", "Docker")),
+                new Employee(4,"Moses",40,"Admin",70000,List.of("Management", "Leadership", "Communication")),
+                new Employee(5,"Jesus",30,"Finance",80000,List.of("Excel", "SQL", "Power BI")),
+                new Employee(6,"Mohammad",38,"IT",90000,List.of("Java", "Spring Boot", "Kafka", "Docker", "Kubernetes"))
+        );
+
+        Optional<Integer> result10 = employees.stream().map(Employee::getId).filter(id -> id == 10).findFirst();
+        System.out.println(result10);
     }
 }
